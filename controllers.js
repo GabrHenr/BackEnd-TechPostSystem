bda1b09a4bdf15d3fdbcde4a4cdc99d956e1d5d7
GabrHenr <gabrielhenriquedxd@gmail.com>
2026-01-09 07:16:01 -0300

    Hotfix: Fixed controllers

--- controllers.js
+++ controllers.js
@@ -1,21 +1,36 @@
 const { Post } = require("./model");
 
 const allPosts = async (req, res) => {
-  const query = await Post.find({}, [
-    "post_title",
-    "teacher_name",
-    "post_creation_date",
-  ]);
-  res.send(query);
+  try {
+    const query = await Post.find({}, [
+      "post_title",
+      "teacher_name",
+      "post_creation_date",
+    ]);
+    res.send(query);
+  } catch {
+    res
+      .status(500)
+      .json({ error: `Error connecting with server to retrieve posts` });
+  }
 };
 
 const searchPosts = async (req, res) => {
-  const regex = new RegExp(req.params.q, "i");
+  try {
+    const regex = new RegExp(req.query.q, "i");
 
-  const searchRegexQuery = await Post.find({
-    $or: [{ post_title: regex }, { post_description: regex }],
-  }).catch((err) => res.send(err));
-  res.send(searchRegexQuery);
+    const searchRegexQuery = await Post.find({
+      $or: [{ post_title: regex }, { post_description: regex }],
+    });
+    if (searchRegexQuery.length === 0) {
+      return res.status(404).json({ success: "No post found" });
+    }
+    res.send(searchRegexQuery);
+  } catch {
+    res
+      .status(500)
+      .json({ erro: "Server error could not connect to the search" });
+  }
 };
 
 const createPosts = async (req, res) => {
@@ -35,29 +50,44 @@ const createPosts = async (req, res) => {
     post_creation_date: new Date(),
     post_video_url: post_video_url,
   });
-
-  await postToCreate
-    .save()
-    .then(res.send("New post Created"))
-    .catch((err) => console.log(err));
+  try {
+    await postToCreate.save();
+    res.status(201).json({ success: "Post created with success" });
+  } catch {
+    res
+      .status(500)
+      .json({ error: "Server error could not connect to create post" });
+  }
 };
 
 const readPost = async (req, res) => {
-  const query = await Post.findById(req.params.id, ["-teacher_id"]);
-  res.send(query.errors);
+  try {
+    const postToRead = await Post.findById(req.params.id, ["-teacher_id"]);
+    res.send(postToRead);
+  } catch {
+    res
+      .status(500)
+      .json({ error: "Server error could not connect to retrieve the post" });
+  }
 };
 
 const editPost = async (req, res) => {
   req.body.post_last_modify_date = new Date();
-  await Post.findByIdAndUpdate(req.params.id, req.body)
-    .then(res.send("Success"))
-    .catch((err) => res.send(err));
+  try {
+    await Post.findByIdAndUpdate(req.params.id, req.body);
+    res.status(201).json({ success: "Edited with success" });
+  } catch {
+    res.status(500).json({ error: "Could not edit post due to server error" });
+  }
 };
 
 const deletePost = async (req, res) => {
-  await Post.findByIdAndDelete(req.params.id)
-    .then(res.send("Deletado"))
-    .catch((err) => res.send(err));
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(201).json({ success: "deleted with success" });
+  } catch {
+    res.status(500).json({ error: "Could not delete due to server error" });
+  }
 };
 
 module.exports = {
